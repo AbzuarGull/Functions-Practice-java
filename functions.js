a1a65c4807fa95e682eeb268e20b971bcd0324bf
Abuzar <abuzargull09@gmail.com>
2023-03-26 16:52:03 +0500

    Functions

---
+++ functions.js
@@ -0,0 +1,51 @@
+//   even & odd data entery programm
+
+// let even = prompt("Enter your number!!!!");
+// if (even % 2 != 0) {
+//     document.write("Your entered number is odd.");
+// }
+// else {
+//     document.write("wrong data entery!!");
+// }
+
+//   functions of even or odd practice
+
+// function oddEven(number) {
+//     let evenOdd = prompt("Enter your number.");
+//     if (evenOdd % 2 == 0) {
+//         document.write("Your enter number is even");
+//     }
+//     else{
+//         document.write("Your enter number is odd!!");
+//     }
+// }
+
+// oddEven();
+
+// write a javascript programm in which we add two number in a function and print the answer in console.log
+
+
+// function sum(number1 , number2) {
+//     let resultAdd = number1 + number2;
+//     console.log("result", resultAdd);
+// }
+// let numb1 =+ prompt("enter your num1");
+// let numb2 =+ prompt("Enter your num2");
+// sum(numb1 ,numb2);
+
+
+// Add two numbers in function
+
+// function addTwoNumbers(a1 ,a2 ) {
+// let addition = a1 + a2;
+// console.log(addition);
+// }
+// let num =+ prompt("enter num");
+// let num1 =+ prompt("enter num1");
+
+
+// addTwoNumbers(num , num1);
+
+
+
+// addTwoNumbers(num , num1);
